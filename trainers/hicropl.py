1f5234fc903de09ecbdc351a421a746181f287ba
Codevidamme <hwiyou2005@gmail.com>
2026-04-15 02:36:31 +0700

    up

--- trainers/hicropl.py
+++ trainers/hicropl.py
@@ -443,7 +443,8 @@ class HiCroPL(TrainerX):
         print("Turning off gradients in both the image and the text encoder")
         name_to_update = "prompt_learner"
         # teacher_ln_names = (".ln_pre.", ".ln_post.", ".ln_1.", ".ln_2.")
-        teacher_ln_names = (".ln_pre.", ".ln_post.")
+        # teacher_ln_names = (".ln_pre.", ".ln_post.")
+        teacher_ln_names = (".ln_pre.", ".ln_post.", ".ln_1.")
         for name, param in self.model.named_parameters():
             trainable = False
 
